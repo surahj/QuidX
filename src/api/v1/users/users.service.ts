b056108a844223f1b5c8e92e03e2b27f7c51a4ab
Opeyemi Odebode <odebodeopeyemi@gmail.com>
2025-03-01 23:30:23 +0100

    Add users endpoint (#37)
    Co-authored-by: surahj <odebodeeopeyemi@gmail.com>

--- src/api/v1/users/users.service.ts
+++ src/api/v1/users/users.service.ts
@@ -1,14 +1,21 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { User, Prisma, Profile, Guest } from '@prisma/postgres/client';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { EmailService } from '@modules/emails/email.service';
+import { PageOptionsDto } from '@common/dto/page-options.dto';
+import { PostgresPrismaService } from '@database/postgres-prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly usersRepository: UsersRepository,
     private readonly emailService: EmailService,
+    private readonly prisma: PostgresPrismaService,
   ) {}
 
   public async createUser(
@@ -73,5 +80,77 @@ export class UsersService {
 
   public async getUserProfile(id: string): Promise<Profile[]> {
     return this.usersRepository.getUserProfile(id) as Promise<Profile[]>;
+  }
+
+  async getAllUsers(adminId: string, options: PageOptionsDto) {
+    const admin = await this.usersRepository.findById(adminId);
+
+    if (admin.role !== 'ADMIN') {
+      throw new ForbiddenException(
+        'You are not allowed to perform this action',
+      );
+    }
+
+    const { skip, take, searchTerm, sortBy, sortDir } = options;
+
+    const where: Prisma.UserWhereInput = {};
+
+    if (searchTerm) {
+      where.AND = [
+        {
+          email: {
+            contains: searchTerm,
+            mode: Prisma.QueryMode.insensitive,
+          },
+        },
+      ];
+    }
+
+    const [users, total] = await Promise.all([
+      this.prisma.user.findMany({
+        where,
+        orderBy: {
+          [sortBy]: sortDir.toLowerCase(),
+        },
+        select: {
+          id: true,
+          email: true,
+          credit: true,
+          bonusCredit: true,
+          isEmailVerified: true,
+          profile: {
+            select: {
+              firstName: true,
+              lastName: true,
+            },
+          },
+          paymentHistories: {
+            select: {
+              id: true,
+              amount: true,
+              status: true,
+              createdAt: true,
+              currency: true,
+              packageId: true,
+            },
+          },
+        },
+        skip,
+        take,
+      }),
+
+      this.prisma.user.count({ where }),
+    ]);
+
+    return {
+      data: users.map((u) => ({
+        ...u,
+        paymentHistories: u.paymentHistories.map((p) => ({
+          ...p,
+          amount: p.amount.toNumber(),
+        })),
+      })),
+      total,
+    };
   }
 }
